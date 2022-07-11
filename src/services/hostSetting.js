9779a16ce895058f8a049e4e677d7284ba593b6a
lijimol-hashroot <92438222+lijimol-hashroot@users.noreply.github.com>
2022-07-11 11:31:00 +0530

    link changed

--- src/services/hostSetting.js
+++ src/services/hostSetting.js
@@ -1,8 +1,8 @@
 var url = '';
 if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
-    url = "https://one.hashroot.com/server";
+    url = "https://one.autowelkin.com/server";
 } else {
-    url = "https://one.hashroot.com/server";
+    url = "https://one.autowelkin.com/server";
 }
 
 export const API_BASE_URL = url;
